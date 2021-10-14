96d48e8a7e4a05060ecfa693fa2244420f231d66
Randy Barents <33863457+randybarents@users.noreply.github.com>
2021-10-14 14:24:02 +0200

    Creation Vue Project

---
+++ starwarsfansite/src/components/ApiRequestTest.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: "get-request",
+  data() {
+    return {
+      totalVuePackages: null
+    };
+  },
+    created() {
+  // Simple GET request using axios
+  axios.get("https://swapi.dev/api/starships/9/")
+    .then(response => this.totalVuePackages = response.data.total);
+    }
+}
+</script>
